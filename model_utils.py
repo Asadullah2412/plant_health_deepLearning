6f232cf00d4ccfe31c632333872eece721d2edfe
Mohammad Asadullah <87116101+Asadullah2412@users.noreply.github.com>
2026-03-07 18:12:33 +0530

    done with docker

--- model_utils.py
+++ model_utils.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+from keras.layers import TFSMLayer
 
-
-model = tf.keras.models.load_model('model/plant_model.h5')
+model = TFSMLayer(
+    "model/plant_model_serving",
+    call_endpoint="serving_default"
+)
 
 class_names = [
 'Potato___Early_blight',
@@ -18,9 +21,15 @@ def predict_image(image):
 
     image = image.resize((224,224))
     image = np.array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
+    image = np.expand_dims(image, axis=0).astype(np.float32)
 
-    prediction = model.predict(image)
+    # Call the model directly (no predict())
+    outputs = model(image)
+
+    # TFSMLayer returns a dict → extract tensor
+    prediction = list(outputs.values())[0]
+
+    prediction = prediction.numpy()
 
     class_index = np.argmax(prediction)
     confidence = float(np.max(prediction))
