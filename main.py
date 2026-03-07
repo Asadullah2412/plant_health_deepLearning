85dee5c91b1d3f30ecea548682615390dc88f3b3
Mohammad Asadullah <87116101+Asadullah2412@users.noreply.github.com>
2026-03-07 15:50:31 +0530

    API created

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image
+import io
+
+from model_utils import predict_image
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {"message": "Plant Disease Detection API running"}
+
+@app.post('/predict')
+async def predict(file:UploadFile = File(...)):
+    contents =await file.read()
+    image = Image.open(io.BytesIO(contents))
+
+    label ,confidence = predict_image(image)
+
+    return{
+        'prediction':label,
+        'confidence': confidence
+    }
